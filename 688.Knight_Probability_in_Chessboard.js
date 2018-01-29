0af4d889e7debd3a786ec04667d15315fe01b41c
Jimkwan <jimkwan.yau@gmail.com>
2018-01-29 23:34:25 +0800

    Add 688

---
+++ 688.Knight_Probability_in_Chessboard.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number} N
+ * @param {number} K
+ * @param {number} r
+ * @param {number} c
+ * @return {number}
+ */
+var knightProbability = function (N, K, r, c) {
+  if (K === 0) return 1;
+  const p = Array(K + 1).fill().map(x => Array(N).fill().map(x => Array(N).fill(0)));
+  let result = 0;
+  for (let i = 0; i <= K; i++) {
+    if (i === 0) {
+      p[i][r][c] = 1;
+    } else {
+      for (let ri = 0; ri < N; ri++) {
+        for (let ci = 0; ci < N; ci++) {
+          for (let mv of [[-2, -1], [-1, -2], [2, -1], [1, -2], [2, 1], [1, 2], [-2, 1], [-1, 2]]) {
+            if (ri - mv[0] < N && ri - mv[0] > -1 && ci - mv[1] < N && ci - mv[1] > -1) {
+              p[i][ri][ci] += p[i - 1][ri - mv[0]][ci - mv[1]] / 8;
+            }
+          }
+          if (i === K) result += p[i][ri][ci];
+        }
+      }
+    }
+  }
+  return result;
+};
+
+console.log(knightProbability(3, 2, 0, 0));
